89981312b0ea45ad1139c1e765c4aefba05a5d9c
SarahSu92 <sarah.sundberg@outlook.com>
2025-07-03 16:58:44 +0200

    (feat):added footer

--- Portfolio/src/components/Footer/Footer.tsx
+++ Portfolio/src/components/Footer/Footer.tsx
@@ -1,5 +1,24 @@
 import './Footer.scss';
+import { HiOutlineMail, HiOutlinePhone } from 'react-icons/hi';
 
 export const Footer = () => {
-    return<></>
-}
+  return (
+    <>
+      <div className="contact-wrapper">
+        <h5 className="form">How to reach me!</h5>
+
+        <div className="contact-footer">
+          <a className="contact-form" href="mailto:sarah.sundberg@outlook.com">
+            <HiOutlineMail size={24} />
+            <span className="contact-spec">sarah.sundberg@outlook.com</span>
+          </a>
+
+          <a className="contact-form" href="tel:+46735669434">
+            <HiOutlinePhone size={24} />
+            <span className="contact-spec">073 566 94 34</span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
